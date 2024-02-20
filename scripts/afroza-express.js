c0ee867da21c26575250c42638eb9390856547d6
ashrafur rahman farhad <itsfaarhaad@gmail.com>
2024-02-20 04:32:55 +0000

    more functionality added

--- scripts/afroza-express.js
+++ scripts/afroza-express.js
@@ -5,17 +5,30 @@ document.addEventListener("DOMContentLoaded", function () {
     let totalSeats = 40;
     let selectedSeats = 0;
 
+    const tableBody = document.querySelector(".table-body");
+
     seats.forEach(seat => {
         seat.addEventListener("click", function () {
             if (seat.classList.contains('selected', 'bg-[#1DD100]', 'text-white')) {
                 seat.classList.remove('selected', 'bg-[#1DD100]', 'text-white');
                 selectedSeats--;
                 totalSeats++;
+
+                removeTableRow(seat.textContent);
             } else {
                 if (selectedSeats < 4) {
                     seat.classList.add('selected', 'bg-[#1DD100]', 'text-white');
                     selectedSeats++;
                     totalSeats--;
+                    const newTableRow = `
+                    <tr">
+                        <td class="px-4 py-2">${seat.innerText}</td>
+                        <td class="px-4 py-2">Economy</td>
+                        <td class="text-right px-4 py-2">550/=</td>
+                    </tr>
+                    `;
+
+                    tableBody.insertAdjacentHTML('beforeend', newTableRow);
                 } else {
                     const modalHTML = `
                         <dialog id="my_modal_1" class="modal">
@@ -50,8 +63,38 @@ document.addEventListener("DOMContentLoaded", function () {
                 // }
 
             }
+
+            const totalPriceRow = document.getElementById('total-price-row');
+
+            if (selectedSeats === 0) {
+                numberOfSelectedSeat.classList.add('hidden')
+                totalPriceRow.classList.add('hidden');
+            } else {
+                numberOfSelectedSeat.classList.add('bg-[#1DD100]', 'text-white');
+                numberOfSelectedSeat.classList.remove('hidden');
+
+                totalPriceRow.classList.remove('hidden');
+            }
+
             numberOfSelectedSeat.innerText = selectedSeats;
             totalSeatDisplay.innerText = totalSeats;
+
+            const totalPriceElement = document.getElementById('total-price');
+            const totalPrice = selectedSeats * 550;
+            totalPriceElement.innerText = totalPrice;
+
         });
     });
+    function removeTableRow(seatName) {
+        var rows = tableBody.querySelectorAll("tr");
+        rows.forEach(function (row) {
+            if (row.querySelector("td:first-child").textContent === seatName) {
+                row.remove();
+            }
+        });
+    }
+
+
+
 });
+
