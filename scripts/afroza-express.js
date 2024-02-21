4e0c5acac4c27add276e0fa468b3f118183f6336
ashrafur rahman farhad <itsfaarhaad@gmail.com>
2024-02-21 00:23:27 +0000

    some changes according to the requirements also comments added

--- scripts/afroza-express.js
+++ scripts/afroza-express.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const nextButton = document.getElementById("next");
 
     function seatClickHandler() {
+        // removing classes on seat deselection, also reducing selected seats total and increasing total seats value. doing just reverse on else block.
         if (this.classList.contains('selected', 'bg-[#1DD100]', 'text-white')) {
             this.classList.remove('selected', 'bg-[#1DD100]', 'text-white');
             selectedSeats--;
@@ -21,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 this.classList.add('selected', 'bg-[#1DD100]', 'text-white');
                 selectedSeats++;
                 totalSeats--;
+                // adding new row after getting data from selected seat
                 const newTableRow = `
                     <tr">
                         <td class="px-4 py-2">${this.innerText}</td>
@@ -31,11 +33,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 tableBody.insertAdjacentHTML('beforeend', newTableRow);
             } else {
+                // modal to display alert on seat selection limit
                 const modalHTML = `
                     <dialog id="my_modal_1" class="modal">
                         <div class="modal-box">
-                            <h3 class="font-bold text-lg">Warning!</h3>
-                            <p class="py-4">You can only select up to 4 seats.</p>
+                            <h3 class="font-bold text-red-600 text-lg">Warning!</h3>
+                            <p class="py-4 text-lg">You can only select up to 4 seats.</p>
                             <div class="modal-action">
                                 <button id="closeButton" class="btn">Close</button>
                             </div>
@@ -60,6 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const totalPriceRow = document.getElementById('total-price-row');
 
+        // updating and styling selected seat number <sup> value
         if (selectedSeats === 0) {
             numberOfSelectedSeat.classList.add('hidden')
             totalPriceRow.classList.add('hidden');
@@ -73,9 +77,9 @@ document.addEventListener("DOMContentLoaded", function () {
         numberOfSelectedSeat.innerText = selectedSeats;
         totalSeatDisplay.innerText = totalSeats;
 
+        // displaying total price
         const totalPriceElement = document.getElementById('total-price');
-        let totalPrice = selectedSeats * 550;
-        totalPriceElement.innerText = parseInt(totalPrice);
+        totalPriceElement.innerText = parseFloat(calculateTotalPrice());
 
 
         toggleNextButton();
@@ -85,6 +89,7 @@ document.addEventListener("DOMContentLoaded", function () {
         seat.addEventListener("click", seatClickHandler);
     });
 
+    // removing table rows upon deselection of the seats
     function removeTableRow(seatName) {
         var rows = tableBody.querySelectorAll("tr");
         rows.forEach(function (row) {
@@ -99,31 +104,37 @@ document.addEventListener("DOMContentLoaded", function () {
     const grandTotalRow = document.getElementById('grand-total-row');
     const grandTotalElement = document.getElementById('grand-total');
     const totalPriceElement = document.getElementById('total-price');
+    const couponForm = document.getElementById('coupon-form');
 
+    // approved coupon and their discount value
     const couponCodes = {
         "NEW15": 0.15,
         "Couple 20": 0.20
     };
 
+    // calculating total price to make grand total calculation easy after coupon apply
     function calculateTotalPrice() {
-        const selectedSeats = parseInt(document.getElementById("number-of-selected-seat").innerText);
+        const selectedSeats = parseFloat(document.getElementById("number-of-selected-seat").innerText);
         const seatPrice = 550;
         return selectedSeats * seatPrice;
     }
 
+    // validating coupon and removing coupon field if it's correct
     function applyCoupon(couponCode) {
-        if (couponCodes.hasOwnProperty(couponCode)) {
+        if (couponCodes.hasOwnProperty(couponCode) && selectedSeats === 4) {
             const discountPercentage = couponCodes[couponCode];
             const totalPrice = calculateTotalPrice();
             const discountedPrice = totalPrice * (1 - discountPercentage);
             grandTotalElement.innerText = discountedPrice.toString();
             grandTotalRow.classList.remove('hidden');
-            couponInput.style.display = 'none';
-            couponApplyButton.style.display = 'none';
+            couponForm.classList.add('hidden');
 
+            // preventing user from selecting seats when a coupon applied and grand total been provided
             seats.forEach(seat => {
                 seat.removeEventListener("click", seatClickHandler);
             });
+        } else {
+            couponForm.classList.remove('hidden');
         }
     }
 
@@ -134,13 +145,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
-
+    // validating provided phone number (11 digit)
     function validatePhoneNumber(phoneNumber) {
-        const phoneNumberRegex = /^\d{10}$/;
+        const phoneNumberRegex = /^\d{11}$/;
         return phoneNumberRegex.test(phoneNumber);
     }
 
-
+    // next button will only be activated when user selected at least one seat and provided a valid phone number
     function toggleNextButton() {
         const phoneNumber = phoneNumberInput.value.trim();
         if (validatePhoneNumber(phoneNumber) && selectedSeats > 0) {
